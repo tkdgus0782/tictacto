32c75f54d13332176d964d86192e6519ed16d925
tkdgus0782 <tkdgus0782@naver.com>
2021-07-06 13:30:45 +0000

    make files

---
+++ src/comp/Board.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Square from './Square';
+
+class Board extends React.Component{
+	
+	
+	render(){
+		return <Square />;
+	}
+}
+
+export default Board;
---
+++ src/comp/Square.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Square extends React.Component{
+	
+	
+	render(){
+		return <div>hello???</div>;
+	}
+}
+
+export default Square;
